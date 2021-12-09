9531e176c0386710a1ee7c10e18aafc5e0558113
Bryan Jacobs <bryan.jacobs@outsystems.com>
2021-12-09 03:13:56 -0700

    completed the testing with essentially a thread.sleep that is more elegant

--- src/test/java/com/example/springbootconsumer/component/MockAbstractConsumer.java
+++ src/test/java/com/example/springbootconsumer/component/MockAbstractConsumer.java
@@ -4,14 +4,36 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 
 @Profile("test")
 @Component
 public class MockAbstractConsumer extends AbstractConsumer {
 
+    public static final Integer NUM_MESSAGES = 1;
+
+    private CountDownLatch latch = new CountDownLatch(NUM_MESSAGES);
+
+    private List<ConsumerRecord<String, String>> consumerRecords = new ArrayList();
+
     @Override
+
     public void consume(ConsumerRecord<String, String> consumerRecord) {
         System.out.println("performing test things: " + consumerRecord.value());
-//            uopKafkaEventConsumerInitializer.shutdown();
+
+        consumerRecords.add(consumerRecord);
+
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    public List<ConsumerRecord<String, String>> getConsumerRecords() {
+        return this.consumerRecords;
     }
 }
--- src/test/java/com/example/springbootconsumer/component/UopKafkaEventConsumerIT.java
+++ src/test/java/com/example/springbootconsumer/component/UopKafkaEventConsumerIT.java
@@ -5,9 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @EmbeddedKafka(brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 @DirtiesContext
@@ -16,24 +22,21 @@ import org.springframework.test.context.ActiveProfiles;
 public class UopKafkaEventConsumerIT {
 
     @Autowired
-    private UopKafkaEventConsumerInitializer uopKafkaEventConsumerInitializer;
-
-    @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @Value("${kafka.topic.in}")
     private String topic;
+
+    @Autowired
+    MockAbstractConsumer abstractConsumer;
 
     @Test
     public void shouldShutdownConsumer() throws InterruptedException {
 
         kafkaTemplate.send(topic, "i'll send an sos to the world");
 
-        // todo: find a way to remove the thread.sleep to get the test to pass
-        Thread.sleep(500);
+        abstractConsumer.getLatch().await(1, TimeUnit.SECONDS);
 
-
-
-
+        assertTrue(abstractConsumer.getConsumerRecords().size() == 1);
     }
 }
